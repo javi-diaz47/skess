738fc73fe9063de3320507a0d7073a4e69d489d6
javi-diaz47 <javiereduardo300@gmail.com>
2026-04-19 23:53:22 -0500

    feat: add colors to the sketch board

--- frontend/src/components/SketchBoard.tsx
+++ frontend/src/components/SketchBoard.tsx
@@ -23,7 +23,23 @@ interface Path {
   color: string
 }
 
+
+export const COLORS = {
+  red: { light: "#f87171", base: "#dc2626", dark: "#991b1b" },
+  orange: { light: "#fb923c", base: "#ea580c", dark: "#9a3412" },
+  yellow: { light: "#facc15", base: "#ca8a04", dark: "#854d0e" },
+  green: { light: "#4ade80", base: "#16a34a", dark: "#166534" },
+  blue: { light: "#60a5fa", base: "#2563eb", dark: "#1e40af" },
+  purple: { light: "#c084fc", base: "#9333ea", dark: "#6b21a8" },
+  pink: { light: "#f472b6", base: "#db2777", dark: "#9d174d" },
+
+  black: { light: "#000", base: "#000", dark: "#000" },
+  white: { light: "#fff", base: "#fff", dark: "#fff" },
+};
+
 export function SketchBoard() {
+
+  const [color, setColor] = useState(COLORS["black"]["base"])
 
   const [paths, setPaths] = useState<Path[]>([])
 
@@ -46,9 +62,10 @@ export function SketchBoard() {
 
     const newPath = new Path2D(lastPath.current)
 
-    setPaths(prev => [...prev, { path: newPath, color: 'green' }])
+    setPaths(prev => [...prev, { path: newPath, color }])
     points.current = []
 
+    svgPath.current.setAttribute("fill", "")
     svgPath.current?.setAttribute("d", "")
   }
 
@@ -63,6 +80,7 @@ export function SketchBoard() {
 
     lastPath.current = pathData
 
+    svgPath.current.setAttribute("fill", color)
     svgPath.current?.setAttribute("d", pathData)
 
   }
@@ -71,8 +89,10 @@ export function SketchBoard() {
     const ctx = canvas.current.getContext('2d')
 
     paths.forEach(cur => {
+      ctx.beginPath()
       ctx.fillStyle = cur.color
       ctx.fill(cur.path)
+      ctx.closePath()
     })
   }
 
@@ -95,20 +115,37 @@ export function SketchBoard() {
   }, [])
 
   return (
-    <div className="relative">
-      <canvas
-        ref={canvas}
-        className="w-96 h-96 bg-background-800"
-        onPointerDown={onPointerDown}
-        onPointerMove={onPointerMove}
-        onPointerUp={onPointerUp}
-      ></canvas>
-      <svg className="w-96 h-96 absolute top-0 pointer-events-none">
-        <g>
-          <path ref={svgPath} />
-        </g>
-      </svg>
+    <>
 
-    </div>
+      <div className="relative">
+        <canvas
+          ref={canvas}
+          className="w-96 h-96 bg-background-100 dark:bg-background-800"
+          onPointerDown={onPointerDown}
+          onPointerMove={onPointerMove}
+          onPointerUp={onPointerUp}
+        ></canvas>
+        <svg className="w-96 h-96 absolute top-0 pointer-events-none">
+          <g>
+            <path ref={svgPath} />
+          </g>
+        </svg>
+      </div>
+      <div className="flex gap-1 p-2 bg-background-100 dark:bg-background-800 rounded-lg w-fit">
+        {Object.entries(COLORS).map(([name, value]) => {
+          const c = value.base;
+
+          return (
+            <button
+              key={name}
+              onClick={() => setColor(c)}
+              className={`w-4 h-4 rounded-full ${color === c ? 'ring-2 ring-accent-200' : ''}`}
+              style={{ backgroundColor: c }}
+              title={name}
+            />
+          );
+        })}
+      </div>
+    </>
   )
 }
