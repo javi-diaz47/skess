b320f869e5038ea7060cd0b4b27d3cc06a374c1e
javi-diaz47 <javiereduardo300@gmail.com>
2026-04-23 09:53:00 -0500

    refactor: implement payload validation function

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,40 +1,13 @@
 from uuid import uuid4
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from src.utils.colors import COLORS
 from src.ws.connection_manager import Connection, ConnectionManager, User
-from src.ws.validation_message import validate_message
+from src.ws.validation_message import validate_message, validate_payload
 import random
 
 app = FastAPI(title="Skess")
 
 manager = ConnectionManager()
-colors = [
-    "red",
-    "orange",
-    "amber",
-    "yellow",
-    "lime",
-    "green",
-    "emerald",
-    "teal",
-    "cyan",
-    "sky",
-    "blue",
-    "indigo",
-    "violet",
-    "purple",
-    "fuchsia",
-    "pink",
-    "rose",
-    "slate",
-    "gray",
-    "zinc",
-    "neutral",
-    "stone",
-    "taupe",
-    "mauve",
-    "mist",
-    "olive",
-]
 
 
 @app.get("/")
@@ -44,7 +17,7 @@ def ping():
 
 @app.websocket("/ws/{client_id}/{client_name}")
 async def websocket_endpoint(ws: WebSocket, client_id: str, client_name: str):
-    conn = Connection(User(client_id, client_name, random.choice(colors)), ws)
+    conn = Connection(User(client_id, client_name, random.choice(COLORS)), ws)
     await manager.connect(conn)
 
     try:
@@ -56,47 +29,9 @@ async def websocket_endpoint(ws: WebSocket, client_id: str, client_name: str):
                 await manager.send_personal_message(conn, val)
                 continue
 
-            if data["type"] == "guess":
-                if "message" not in data["payload"]:
-                    await manager.send_personal_message(
-                        conn,
-                        {
-                            "error": "Invalid Message",
-                            "message": "Payload must include a property message of type string",
-                        },
-                    )
-                    continue
-
-            elif data["type"] == "sketch":
-                if "path" not in data["payload"]:
-                    await manager.send_personal_message(
-                        conn,
-                        {
-                            "error": "Invalid Message",
-                            "message": "Payload must include a property path of type string",
-                        },
-                    )
-                    continue
-                if "color" not in data["payload"]:
-                    await manager.send_personal_message(
-                        conn,
-                        {
-                            "error": "Invalid Message",
-                            "message": "Payload must include a string property color representing the path’s hexadecimal color.",
-                        },
-                    )
-                    continue
-                if "sketching" not in data["payload"]:
-                    await manager.send_personal_message(
-                        conn,
-                        {
-                            "error": "Invalid Message",
-                            "message": "Payload must include a boolean property sketching indicating whether the path is finished.",
-                        },
-                    )
-                    continue
-
-            else:
+            val = validate_payload(data)
+            if val["error"]:
+                await manager.send_personal_message(conn, val)
                 continue
 
             event_id = str(uuid4())
