e5722ff608de2a7c462e363894a0018ce3e28499
javi-diaz47 <javiereduardo300@gmail.com>
2026-05-04 15:56:01 -0500

    fix: add max width in home page

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -8,23 +8,25 @@ import { Header } from "./components/Header";
 export function Home() {
 
   return (
-    <main className="max-w-8xl bg-background-50 text-text-900 dark:bg-background-950 dark:text-text-50 p-8">
-      <Header />
-      <div className="grid grid-rows-3 md:grid-cols-2 md:grid-rows-2 lg:grid-flow-col lg:grid-cols-7 lg:grid-rows-2 gap-4">
-        <div className="md:col-span-2 lg:col-span-5 lg:row-span-2 relative">
-          <SketchBoard />
-          <div className="absolute left-1/2 top-1/2 -translate-1/2">
-            <WordSelector />
+    <div className="flex justify-center">
+      <main className="w-full max-w-[1800px] bg-background-50 text-text-900 dark:bg-background-950 dark:text-text-50 p-8">
+        <Header />
+        <div className="grid grid-rows-3 md:grid-cols-2 md:grid-rows-2 lg:grid-flow-col lg:grid-cols-7 lg:grid-rows-2 gap-4">
+          <div className="md:col-span-2 lg:col-span-5 lg:row-span-2 relative">
+            <SketchBoard />
+            <div className="absolute left-1/2 top-1/2 -translate-1/2">
+              <WordSelector />
+            </div>
+          </div>
+          <div className="lg:col-span-2">
+            <Chat />
+          </div>
+          <div className="lg:col-span-2">
+            <Leaderboard />
           </div>
         </div>
-        <div className="lg:col-span-2">
-          <Chat />
-        </div>
-        <div className="lg:col-span-2">
-          <Leaderboard />
-        </div>
-      </div>
-      <PlayButton />
-    </main>
+        <PlayButton />
+      </main>
+    </div>
   )
 }
