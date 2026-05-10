e99ca3a194ec1a4ec650f685db980976d504f111
javi-diaz47 <javiereduardo300@gmail.com>
2026-05-10 13:31:30 -0500

    feat: remove timer from chat

--- frontend/src/hooks/useChat.tsx
+++ frontend/src/hooks/useChat.tsx
@@ -1,8 +1,6 @@
 import { useContext, useEffect, useState } from "react"
 import { WebSocketContext, type GuessSocketEvent } from "../context/WebsSocketsContext"
 import { SessionContext } from "../context/SessionContext"
-import { useTimer } from "./useTimer"
-import { ONE_SECOND_IN_MILISECONDS } from "../contants/secondsToMiliseconds"
 
 export const useChat = () => {
 
@@ -11,11 +9,8 @@ export const useChat = () => {
 
   const [messages, setMessages] = useState<GuessSocketEvent[]>([])
 
-  const [status, setStatus] = useState("end")
-  const { time, startTimer, cancelTimer } = useTimer()
 
   const sendMessage = (message: string) => {
-    if (status === "guess" && (time === null || time === 0)) return;
     send({
       "type": "guess",
       "payload": {
@@ -49,38 +44,15 @@ export const useChat = () => {
       setMessages(prev => [...prev, message])
     })
 
-    const unsubStatus = subscribe("status", (ev) => {
-      if (ev.payload.status === "start") {
-        setMessages([])
-      }
-      if (ev.payload.status === "guess") {
-        const timestamp = new Date(ev.timestamp * ONE_SECOND_IN_MILISECONDS)
-        const now = new Date()
-
-        const diff = (now.getTime() - timestamp.getTime()) / ONE_SECOND_IN_MILISECONDS
-
-        const leftTime = Math.round(ev.game_guess_limit - diff)
-
-        if (leftTime > 0) {
-          startTimer(leftTime)
-        }
-      }
-      if (ev.payload.status === "end") {
-        cancelTimer()
-      }
-      setStatus(ev.payload.status)
-    })
 
     return () => {
       unsubMessage()
       unsubClose()
-      unsubStatus()
     }
 
   }, [])
 
   return {
-    time,
     messages,
     sendMessage
   }
