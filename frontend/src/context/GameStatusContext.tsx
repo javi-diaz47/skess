d2f174da67b946501ba27ceb1472fa4f5c6d9198
javi-diaz47 <javiereduardo300@gmail.com>
2026-06-06 00:20:37 -0500

    feat: add sound fx when a user join or leave a room

--- frontend/src/context/GameStatusContext.tsx
+++ frontend/src/context/GameStatusContext.tsx
@@ -1,76 +1,151 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { WebSocketContext } from "./WebsSocketsContext";
-import type { UserSession } from "./SessionContext";
+import { createContext, useContext, useEffect, useRef, useState } from 'react'
+import {
+  WebSocketContext,
+  type GameStarted,
+  type GameUpdated,
+  type HintRevealed,
+  type TurnEnded,
+  type UserWebSocket,
+  type WordSelected,
+} from './WebSockets/WebsSocketsContext'
+import type { GamePaused } from './WebSockets/types'
 
 type Status = {
-  state: "start" | "guess" | "end" | "hint"
-  sketcher: UserSession
+  state: 'start' | 'guess' | 'end' | 'hint' | 'pause'
+  sketcher: UserWebSocket
   timestamp: number | null
-  game_guess_limit: number | null
+  guess_limit: number | null
   guess_word: string
   hint: string
   word_letter_count: number
-  game_round: number | null
-  game_max_round: number | null
-  game_turn: number | null
-  game_max_turn: number | null
+  round: number | null
+  max_rounds: number | null
+  turn: number | null
+  max_turns: number | null
 }
 
 export type GameStatusContextValue = {
   status: Status
 }
 
-export const GameStatusContext = createContext<GameStatusContextValue | null>(null)
+export const GameStatusContext = createContext<GameStatusContextValue | null>(
+  null,
+)
 
-export const GameStatusProvider = ({ children }) => {
-
+export const GameStatusProvider = ({ children }: { children: string }) => {
   const { subscribe } = useContext(WebSocketContext)
+
+  const logoutAudio = useRef<HTMLAudioElement>(null)
+  const loginAudio = useRef<HTMLAudioElement>(null)
 
   const [status, setStatus] = useState<Status | null>(null)
 
-
   useEffect(() => {
+    loginAudio.current = new Audio('/sounds/player-join.mp3')
+    logoutAudio.current = new Audio('/sounds/player-leave.mp3')
 
-    const unsubstatus = subscribe("status", (ev) => {
+    const unsubPlayerAbandoned = subscribe('player_abandoned', () => {
+      logoutAudio.current?.play()
+    })
 
-      setStatus(prev => {
-        console.log(ev.payload.status, ev)
+    const unsubPlayerJoined = subscribe('player_joined', () => {
+      loginAudio.current?.play()
+    })
 
-        if (ev?.payload?.status === "start") {
-          return ({
-            ...prev,
-            state: ev?.payload?.status,
-            sketcher: ev?.payload?.sketcher,
-            timestamp: ev.timestamp,
-            game_guess_limit: ev.game_guess_limit,
-            hint: ev?.payload?.hint,
-            word_letter_count: ev?.payload?.word_letter_count,
-            game_round: ev?.game_round,
-            game_max_round: ev?.game_max_round,
-            game_turn: ev?.game_turn,
-            game_max_turn: ev?.game_max_turn
-          })
-        }
-
-        if (ev?.payload?.status === "hint") {
-          return ({ ...prev, state: ev?.payload?.status, hint: ev?.payload?.hint, word_letter_count: ev?.payload?.word_letter_count })
-        }
-
-        return ({
+    const unsubWordSelected = subscribe('word_selected', (ev: WordSelected) => {
+      setStatus((prev) => {
+        return {
           ...prev,
-          state: ev?.payload?.status,
-          sketcher: ev?.payload?.sketcher,
+          state: 'guess',
+          sketcher: ev.sketcher,
           timestamp: ev.timestamp,
-          game_guess_limit: ev.game_guess_limit,
-          hint: ev?.payload?.hint,
-          word_letter_count: ev?.payload?.word_letter_count,
-        })
+          guess_limit: ev.guess_limit,
+          hint: ev.hint,
+          word_letter_count: ev.word_letter_count,
+        }
       })
     })
 
+    const unsubHintRevealed = subscribe('hint_revealed', (ev: HintRevealed) => {
+      setStatus((prev) => {
+        return {
+          ...prev,
+          state: prev?.state === 'pause' ? 'pause' : 'hint',
+          hint: ev.hint,
+          word_letter_count: ev.word_letter_count,
+        }
+      })
+    })
 
-    return () => unsubstatus()
+    const unsubTurnEnded = subscribe('turn_ended', (ev: TurnEnded) => {
+      setStatus((prev) => {
+        return {
+          ...prev,
+          state: prev?.state === 'pause' ? 'pause' : 'end',
+          hint: ev.hint,
+          word_letter_count: ev.word_letter_count,
+          timestamp: ev.timestamp,
+        }
+      })
+    })
 
+    const unsubGameStarted = subscribe('game_started', (ev: GameStarted) => {
+      setStatus((prev) => {
+        return {
+          ...prev,
+          state: 'start',
+          round: ev.round,
+          max_rounds: ev.max_rounds,
+          turn: ev.turn,
+          max_turns: ev.max_turns,
+        }
+      })
+    })
+
+    const unsubGameUpdated = subscribe('game_updated', (ev: GameUpdated) => {
+      console.log('GAME UPDATED', ev)
+      setStatus((prev) => {
+        return {
+          state: prev?.state === 'pause' ? 'pause' : 'guess',
+
+          sketcher: ev.sketcher,
+          timestamp: ev.timestamp,
+
+          hint: ev.hint,
+          word_letter_count: ev.word_letter_count,
+
+          round: ev.round,
+          max_rounds: ev.max_rounds,
+
+          turn: ev.turn,
+          max_turns: ev.max_turns,
+
+          guess_limit: ev.guess_limit,
+
+          leaderboard: ev.leaderboard,
+        }
+      })
+    })
+
+    const unsubGamePaused = subscribe('game_paused', (_: GamePaused) => {
+      setStatus((prev) => {
+        return {
+          ...prev,
+          state: 'pause',
+        }
+      })
+    })
+
+    return () => {
+      unsubWordSelected()
+      unsubGameStarted()
+      unsubHintRevealed()
+      unsubTurnEnded()
+      unsubGameUpdated()
+      unsubGamePaused()
+      unsubPlayerAbandoned()
+      unsubPlayerJoined()
+    }
   }, [])
 
   return (
