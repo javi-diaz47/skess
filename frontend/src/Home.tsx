6dbab8e42f5f045e8ca03165c297cc5654ec05e6
javi-diaz47 <javiereduardo300@gmail.com>
2026-06-06 00:08:12 -0500

    feat: add loading animation in status bar

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,21 +1,20 @@
-import { Chat } from "./components/Chat";
-import { SketchBoard } from "./components/SketchBoard"
-import { Leaderboard } from "./components/Leaderboard";
-import { WordSelector } from "./components/WordSelector";
-import { PlayButton } from "./components/PlayButtton";
-import { Header } from "./components/Header";
-import { StatusBar } from "./components/StatusBar";
-import { ChatInput } from "./components/ChatInput";
+import { Chat } from './components/Chat'
+import { SketchBoard } from './components/SketchBoard'
+import { Leaderboard } from './components/Leaderboard'
+import { WordSelector } from './components/WordSelector'
+import { PlayButton } from './components/PlayButtton'
+import { Header } from './components/Header'
+import { StatusBar } from './components/StatusBar'
+import { ChatInput } from './components/ChatInput'
 
 export function Home() {
-
   return (
-    <main className="w-full max-w-7xl grid p-2
+    <main
+      className="w-full max-w-7xl grid p-2
       grid-cols-2 grid-rows-[auto_auto_1fr_minmax(0,1fr)_auto] gap-2
       md:grid-rows-[3rem_4rem_3fr_2fr_auto] md:grid-cols-[1fr_1fr] md:gap-4 md:p-4
       lg:max-h-3/4 lg:grid-rows-[3rem_4rem_1fr_1fr_auto] lg:grid-cols-[3fr_1fr] lg:gap-6
     ">
-
       <section className="col-span-2">
         <Header />
       </section>
@@ -42,8 +41,6 @@ export function Home() {
       <section className="col-span-2 lg:col-span-1">
         <ChatInput />
       </section>
-
     </main>
-
   )
 }
--- frontend/src/components/StatusBar.tsx
+++ frontend/src/components/StatusBar.tsx
@@ -1,78 +1,81 @@
-import { useContext, useEffect } from "react"
-import { useTimer } from "../hooks/useTimer"
-import { ONE_SECOND_IN_MILISECONDS } from "../contants/secondsToMiliseconds"
-import { Timer } from "./Timer"
-import { GameStatusContext } from "../context/GameStatusContext"
-import { CHAT_COLORS } from "../contants/chatColors"
+import { useContext, useEffect } from 'react'
+import { useTimer } from '../hooks/useTimer'
+import { ONE_SECOND_IN_MILISECONDS } from '../contants/secondsToMiliseconds'
+import { Timer } from './Timer'
+import { GameStatusContext } from '../context/GameStatusContext'
+import { CHAT_COLORS } from '../contants/chatColors'
+import { LoaderAnimation } from './LoaderAnimation'
 
 export function StatusBar() {
-
   const { time, startTimer, cancelTimer } = useTimer()
 
   const { status } = useContext(GameStatusContext)
 
   useEffect(() => {
-
-    if (status?.state === "guess") {
-
+    if (status?.state === 'guess') {
       const timestamp = new Date(status?.timestamp * ONE_SECOND_IN_MILISECONDS)
       const now = new Date()
 
-      const diff = (now.getTime() - timestamp.getTime()) / ONE_SECOND_IN_MILISECONDS
+      const diff =
+        (now.getTime() - timestamp.getTime()) / ONE_SECOND_IN_MILISECONDS
 
-      const leftTime = Math.round(status?.game_guess_limit - diff)
+      const leftTime = Math.round(status?.guess_limit - diff)
 
+      console.log(leftTime, ' <- LEFT TIME')
       if (leftTime > 0) {
         startTimer(leftTime)
       }
-
     }
 
-    if (status?.state === "end") {
+    if (status?.state === 'end') {
       cancelTimer()
     }
-
   }, [status])
-
 
   return (
     <section className="flex text-sm md:text-base text-text-900 dark:text-text-50 justify-between md:justify-center items-center gap-2 md:gap-8">
-
       <div className="min-w-19 md:h-16 flex gap-2 justify-center items-center text-sm md:text-base font-bold bg-background-100 dark:bg-background-900 rounded-full py-2 md:py-4 px-4 md:px-8">
-        <p>{status?.game_round || 0}/{status?.game_max_round || 0}</p>
+        <p>
+          {status?.round || 0}/{status?.max_rounds || 0}
+        </p>
         <p className="hidden md:block">Rounds</p>
       </div>
 
-      {
-        status?.hint && (
-          <div className="md:h-16 flex justify-center gap-1 md:gap-4 md:text-2xl font-bold bg-background-100 dark:bg-background-900 rounded-full py-2 md:py-4 px-4 md:px-8">
-            <p>
-              {
-                status?.hint.split("").map((ch, i) => (
-                  <span key={`${i}-${ch}`} className="mr-1 md:mr-2">{ch}</span>
-                ))
-              }
-            </p>
-            <span className="self-end text-xs md:text-base">
-              {status?.word_letter_count}
-            </span>
-          </div>
-        )
-      }
+      {status?.state === 'pause' && (
+        <div className="md:h-16 flex justify-center items-center gap-6 md:gap-4 font-bold bg-background-100 dark:bg-background-900 rounded-full py-2 md:py-4 px-4 md:px-8">
+          <p className="shimmer-text fot-bold italic">
+            Waiting for players to join
+          </p>
+          <LoaderAnimation className="w-12 stroke-accent-500" />
+        </div>
+      )}
+
+      {status?.hint && status?.state !== 'pause' && (
+        <div className="md:h-16 flex justify-center gap-1 md:gap-4 md:text-2xl font-bold bg-background-100 dark:bg-background-900 rounded-full py-2 md:py-4 px-4 md:px-8">
+          <p>
+            {status?.hint.split('').map((ch, i) => (
+              <span key={`${i}-${ch}`} className="mr-1 md:mr-2">
+                {ch}
+              </span>
+            ))}
+          </p>
+          <span className="self-end text-xs md:text-base">
+            {status?.word_letter_count}
+          </span>
+        </div>
+      )}
 
       <Timer time={time} className="" />
 
-      {
-        status?.sketcher && (
-          <p className="hidden break-all md:flex items-center max-w-72 h-16 text-sm md:text-base bg-background-100 dark:bg-background-900 rounded-full py-4 px-8">
-            <span className={`mr-2 font-bold ${CHAT_COLORS[status?.sketcher?.color]}`}>
-              {status?.sketcher?.name}
-            </span>
-            is sketching!
-          </p>
-        )
-      }
-
+      {status?.sketcher && status?.state !== 'pause' && (
+        <p className="hidden break-all md:flex items-center max-w-72 h-16 text-sm md:text-base bg-background-100 dark:bg-background-900 rounded-full py-4 px-8">
+          <span
+            className={`mr-2 font-bold ${CHAT_COLORS[status?.sketcher?.color]}`}>
+            {status?.sketcher?.name}
+          </span>
+          is sketching!
+        </p>
+      )}
     </section>
   )
 }
