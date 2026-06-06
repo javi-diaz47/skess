fd7301f2982522835080910b6c45276de7ef64fb
javi-diaz47 <javiereduardo300@gmail.com>
2026-06-06 00:06:24 -0500

    feat: add support for WebSocket-based file distribution in leaderboard and word selector

--- frontend/src/components/Leaderboard.tsx
+++ frontend/src/components/Leaderboard.tsx
@@ -1,66 +1,84 @@
-import { useContext, useEffect, useState } from "react"
-import { WebSocketContext, type UserAPI } from "../context/WebsSocketsContext"
-import { CHAT_COLORS } from "../contants/chatColors"
-import { SessionContext } from "../context/SessionContext"
+import { useContext, useEffect, useState } from 'react'
+import {
+  WebSocketContext,
+  type LeaderboardUpdated,
+  type UserAPI,
+  type UserWebSocket,
+} from '../context/WebSockets/WebsSocketsContext'
+import { CHAT_COLORS } from '../contants/chatColors'
+import { SessionContext } from '../context/SessionContext'
 
 export function Leaderboard() {
-
   const { session } = useContext(SessionContext)
   const { subscribe } = useContext(WebSocketContext)
 
-  const [leaderboard, setLeaderboard] = useState<UserAPI[]>([])
+  const [leaderboard, setLeaderboard] = useState<UserWebSocket[]>([])
 
   useEffect(() => {
-    const unsubLeaderboard = subscribe("leaderboard", (ev) => {
-      setLeaderboard(ev.payload.leaderboard)
-    })
+    const unsubLeaderboard = subscribe(
+      'leaderboard_updated',
+      (ev: LeaderboardUpdated) => {
+        setLeaderboard(ev.leaderboard)
+      },
+    )
 
     return () => {
       unsubLeaderboard()
     }
-
   }, [])
 
   const topLeadersColors = (position: number) => {
+    if (position == 1) return 'bg-amber-500 text-text-950'
+    if (position == 2) return 'bg-slate-500 text-text-50'
+    if (position == 3) return 'bg-accent-500 text-text-50'
 
-    if (position == 1) return "bg-amber-500 text-text-950"
-    if (position == 2) return "bg-slate-500 text-text-50"
-    if (position == 3) return "bg-accent-500 text-text-50"
-
-    return "bg-background-800 text-text-300"
+    return 'bg-background-800 text-text-300'
   }
 
   return (
     <div className="flex flex-col h-full min-h-0 flex-1">
-
       <div className="flex-1 bg-background-100 dark:bg-background-900 overflow-y-scroll p-2 rounded-2xl">
-
         <div className="flex items-center gap-2 mb-2">
-          <svg className="w-5 h-5 text-accent-500" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" >
+          <svg
+            className="w-5 h-5 text-accent-500"
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4l4 -6" />
           </svg>
-          <h2 className="md:text-2xl font-bold text-text-400 dark:text-text-200">Leaderboard</h2>
+          <h2 className="md:text-2xl font-bold text-text-400 dark:text-text-200">
+            Leaderboard
+          </h2>
         </div>
 
         <ul className="text-sm md:text-base">
-          {
-            leaderboard && leaderboard.map((user, i) => (
+          {leaderboard &&
+            leaderboard.map((user, i) => (
               <li className="flex items-center justify-between p-2">
-                <div className={`mr-2 font-bold ${CHAT_COLORS[user.color]} flex gap-2 items-center`}>
-                  <div className={`${topLeadersColors(i + 1)} rounded-full w-6 h-6 flex justify-center items-center`}>
+                <div
+                  className={`mr-2 font-bold ${CHAT_COLORS[user.color]} flex gap-2 items-center`}>
+                  <div
+                    className={`${topLeadersColors(i + 1)} rounded-full w-6 h-6 flex justify-center items-center`}>
                     {i + 1}
                   </div>
-                  <span className={`${session.id === user.id ? "text-accent-400" : ""}`}>
+                  <span
+                    className={`${session.id === user.id ? 'text-accent-400' : ''}`}>
                     {user.name}
                   </span>
                 </div>
-                <span className={`${session.id === user.id ? "text-accent-400" : ""}`}>
+                <span
+                  className={`${session.id === user.id ? 'text-accent-400' : ''}`}>
                   {user.score}
                 </span>
               </li>
-            ))
-          }
+            ))}
         </ul>
       </div>
     </div>
--- frontend/src/components/WordSelector.tsx
+++ frontend/src/components/WordSelector.tsx
@@ -1,74 +1,78 @@
-import { useContext, useEffect, useState } from "react"
-import { WebSocketContext } from "../context/WebsSocketsContext"
-import { useTimer } from "../hooks/useTimer"
-import { Timer } from "./Timer"
+import { useContext, useEffect, useState } from 'react'
+import {
+  WebSocketContext,
+  type CreateSelectWord,
+  type WordSelectionStarted,
+} from '../context/WebSockets/WebsSocketsContext'
+import { useTimer } from '../hooks/useTimer'
+import { Timer } from './Timer'
 
 export function WordSelector() {
-  const [choose, setChoose] = useState([])
+  const [choose, setChoose] = useState<string[]>([])
   const { subscribe, send } = useContext(WebSocketContext)
 
   const automaticChooseWord = () => {
-    send({
-      type: "choose_selection",
-      payload: {
-        word: choose[0]
-      }
-    })
-    setChoose([])
+    const newEv: CreateSelectWord = {
+      type: 'select_word',
+      word: choose[0],
+    }
+    send(newEv)
 
+    setChoose([])
   }
 
-  const { time, startTimer, cancelTimer } = useTimer({ onEndCallback: automaticChooseWord })
+  const { time, startTimer, cancelTimer } = useTimer({
+    onEndCallback: automaticChooseWord,
+  })
 
   const onChooseWord = (word: string) => {
-    send({
-      type: "choose_selection",
-      payload: {
-        word
-      }
-    })
+    const newEv: CreateSelectWord = {
+      type: 'select_word',
+      word,
+    }
+    send(newEv)
+
     cancelTimer()
     setChoose([])
   }
 
-
   useEffect(() => {
-    const unsubChoose = subscribe("choose_options", (ev) => {
-      setChoose(() => ev.payload.words)
-      startTimer(10)
-    })
+    const unsubChoose = subscribe(
+      'word_selection_started',
+      (ev: WordSelectionStarted) => {
+        setChoose(() => ev.words)
+        startTimer(ev.timer)
+      },
+    )
 
     return () => {
       unsubChoose()
     }
   }, [])
 
-
   return (
     <div>
-      {
-        choose && choose.length == 3 && (
-          <div className="flex flex-col items-center gap-8 p-8 rounded-4xl border-2 bg-background-50 dark:bg-background-900  border-background-200 dark:border-background-400 shadow-sm md:shadow-lg shadow-background-200 dark:shadow-background-400">
-            <h2 className="text-4xl font-bold text-center">Choose a word</h2>
+      {choose && choose.length == 3 && (
+        <div className="flex flex-col items-center gap-8 p-8 rounded-4xl border-2 bg-background-50 dark:bg-background-900  border-background-200 dark:border-background-400 shadow-sm md:shadow-lg shadow-background-200 dark:shadow-background-400">
+          <h2 className="text-4xl font-bold text-center">Choose a word</h2>
 
-            <Timer time={time} className="bg-background-100 dark:bg-background-800" />
+          <Timer
+            time={time}
+            className="bg-background-100 dark:bg-background-800"
+          />
 
-            <div className="flex flex-col gap-2 md:flex-row md:gap-8">
-              {
-                choose.map((word, i) => (
-                  <button
-                    key={`${word}-${i}`}
-                    onClick={() => onChooseWord(word)}
-                    className="min-w-42 h-12 md:h-48 text-xl first-letter:capitalize bg-linear-0 from-20% from-background-100 to-background-50 dark:from-background-800 dark:to-background-900 font-bold cursor-pointer hover:scale-125 active:scale-100 duration-125 rounded-2xl border-2 border-background-200 dark:border-background-400 shadow-sm md:shadow-lg shadow-background-200 dark:shadow-background-500"
-                  >
-                    {word}
-                  </button>
-                ))
-              }
-            </div>
+          <div className="flex flex-col gap-2 md:flex-row md:gap-8">
+            {choose.map((word, i) => (
+              <button
+                key={`${word}-${i}`}
+                onClick={() => onChooseWord(word)}
+                className="min-w-42 h-12 md:h-48 text-xl first-letter:capitalize bg-linear-0 from-20% from-background-100 to-background-50 dark:from-background-800 dark:to-background-900 font-bold cursor-pointer hover:scale-125 active:scale-100 duration-125 rounded-2xl border-2 border-background-200 dark:border-background-400 shadow-sm md:shadow-lg shadow-background-200 dark:shadow-background-500">
+                {word}
+              </button>
+            ))}
           </div>
-        )
-      }
-    </div >
+        </div>
+      )}
+    </div>
   )
 }
