6d25c590d3cace7ec8fb24ab43ce2b016f393b89
javi-diaz47 <javiereduardo300@gmail.com>
2026-06-08 13:39:32 -0500

    refactor: add useCallback to useTimer hook

--- frontend/src/hooks/useTimer.tsx
+++ frontend/src/hooks/useTimer.tsx
@@ -1,40 +1,54 @@
-import { useEffect, useRef, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 interface UseTimer {
   onEndCallback?: () => void
 }
 
 export function useTimer(props?: UseTimer) {
-
-  const [time, setTime] = useState<number | null>(null)
+  const [time, setTime] = useState<number>(0)
   const interval = useRef<number | undefined>(undefined)
+  const callbackRef = useRef(props?.onEndCallback)
 
-  const startTimer = (time: number) => {
-    setTime(time)
-    interval.current = setInterval(() => {
-      setTime(prev => prev - 1)
-    }, 1000)
-  }
-
-  const cancelTimer = () => {
+  const clearTime = () => {
     clearInterval(interval.current)
-    setTime(null)
+    interval.current = undefined
   }
 
+  const cancelTimer = useCallback(() => {
+    clearTime()
+    setTime(0)
+  }, [])
 
+  const startTimer = useCallback(
+    (time: number) => {
+      cancelTimer()
+      setTime(time)
+      interval.current = setInterval(() => {
+        setTime((prev) => Math.max(prev - 1, 0))
+      }, 1000)
+    },
+    [cancelTimer],
+  )
+
+  // remove the interval when component unmounts
   useEffect(() => {
     return () => {
       cancelTimer()
     }
-  }, [])
+  }, [cancelTimer])
 
+  // update the callback function each time it receives a new one
   useEffect(() => {
-    if (time === 0) {
-      props?.onEndCallback()
-      cancelTimer()
-    }
-  }, [time])
+    callbackRef.current = props?.onEndCallback
+  }, [props])
 
+  // Execute onEndCallback and remove time interval
+  useEffect(() => {
+    if (time !== 0) return
+
+    callbackRef.current?.()
+    clearTime()
+  }, [time])
 
   return {
     time: time ?? 0,
